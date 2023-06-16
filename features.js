54ead9258583155f31b54b76cec610443409c95c
Akash Vaishnav <akashvaishnav9897@gmial.com>
2023-06-16 09:48:39 +0530

    initial commit

---
+++ features.js
@@ -0,0 +1,7 @@
+const myName="Mr.Akashviashnav";
+export const myNumber="8193849780";
+export const faname="shiv-kumar";
+
+// module.exports = gfName;
+export default myName;
+// export {myNumber, faname};
